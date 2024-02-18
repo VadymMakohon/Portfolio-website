a5abbb92a23dcb79abd8a460fc94b1c3c5a3523e
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-18 14:24:40 +0100

    simple updates

---
+++ simple-js-app/js/script.js
@@ -0,0 +1,8 @@
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+//single line comment
+
+/* This is a longer comment.
+Example of how to use a comment over multiple lines. */
