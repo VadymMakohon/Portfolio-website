6e9b0d3be1f4848d860b09c70d1bfc14faaab517
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-20 10:28:21 +0100

    Update script.js
    Updating the list inside the array

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,24 +1,18 @@
-// Create a new variable called pokemonList and assign a blank array to it
-var pokemonList = [];
-
-// Add Pokémon objects (at list 3) to the array
-pokemonList.push({
-  name: "Venusaur",
-  height: 7,
-  types: ['grass', 'poison']
-});
-
-pokemonList.push({
-  name: "Charmander",
-  height: 6,
-  types: ['fire']
-});
-
-pokemonList.push({
-  name: "Squirtle",
-  height: 5,
-  types: ['water']
-});
-
-// Display the pokemonList array
-console.log(pokemonList);
+let pokemonList = [
+    {
+      name: "Venusaur",
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: "Charmander",
+      height: 6,
+      types: ['fire']
+    },
+    {
+      name: "Squirtle",
+      height: 5,
+      types: ['water']
+    }
+    ];
+    
